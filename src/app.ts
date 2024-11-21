85006e7b790f109b060817dc1870bb70c6e5f623
jahidhasan136 <jh3495689@gmail.com>
2024-11-21 08:12:44 +0600

    added all code

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './modules/students/student.route';
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+//application routes
+app.use('/api/v1/students', StudentRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
-});
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World');
+};
+
+app.get('/', getAController);
 
 export default app;
